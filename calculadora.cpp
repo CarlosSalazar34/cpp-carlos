7bcac8f9578ae6a459110c7f6aefd14b5e6097fd
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-03-05 23:36:46 -0400

    finish the calculator

--- calculadora.cpp
+++ calculadora.cpp
@@ -20,9 +20,9 @@ int main()
         cout << "Ingrese una opcion: \n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir \n 5. Salir" << endl;
         cout << "Opcion: ";
         cin >> option;
+        int a, b;
         if (option == 1)
         {
-            int a, b;
             cout << "Ingrese el primer numero: ";
             cin >> a;
             cout << "Ingrese el segundo numero: ";
@@ -31,7 +31,39 @@ int main()
 
         }else if (option == 2)
         {
-            
+            cout << "Ingrese el primer numero: ";
+            cin >> a;
+            cout << "Ingrese el segundo numero: ";
+            cin >>b;
+            cout << "El resultado de la resta es: " << a - b << endl;
+        }
+        else if (option == 3)
+        {
+            cout << "Ingrese el primer numero: ";
+            cin >> a;
+            cout << "Ingrese el segundo numero: ";
+            cin >>b;
+            cout << "El resultado de la multiplicacion es: " << a * b << endl;
+        }
+        else if (option == 4)
+        {
+            cout << "Ingrese el primer numero: ";
+            cin >> a;
+            cout << "Ingrese el segundo numero: ";
+            cin >>b;
+            if (b != 0)
+                cout << "El resultado de la division es: " << a / b << endl;
+            else
+                cout << "Error: No se puede dividir por cero." << endl;
+        }
+        else if (option == 5)
+        {
+            cout << "Gracias por usar la calculadora. ¡Hasta luego!" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Opcion no valida. Por favor, intente de nuevo." << endl;
         }
 
     }
