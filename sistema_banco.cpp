8500adb2df5cf7153860cb10e13a3301744580da
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-03-20 10:05:57 -0400

    init the bank system

--- sistema_banco.cpp
+++ sistema_banco.cpp
@@ -1,35 +1,11 @@
-#include <vector>
-#include <map>
-#include <any>
-#include <string>
-#include <cstdio>
+#include <iostream>
 using namespace std;
 
-class SistemaBanco{
-private:
-    vector<map<string, any>> clientes;
-    long long reservas = 200000000000;
-
-public:
-    SistemaBanco(const vector<map<string, any>>& c): clientes(c) {}
-    SistemaBanco() = default;
-
-    void iniciar(){ 
-        printf("Bienvenido al sistema del banco\n");
-    }
-};
-
 int main(){ 
-    vector<map<string, any>> clientes = {
-        { 
-            {"name", string("Carlos")},
-            {"edad", 12},
-            {"balance", 25000}
-        }
-    }; 
-
-    SistemaBanco sistema(clientes);
-    sistema.iniciar();
-
+    cout << "BIENVENIDO AL SISTEMA DE BANCO" <<
+        "n1. Crear cuenta" << endl <<
+        "n2. Consultar saldo" << endl <<
+        "n3. Retirar dinero" << endl <<
+        "n4. Depositar dinero"<< endl;
     return 0;
 }
