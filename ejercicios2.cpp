2baa858e8e4e7518c1512a4771fd93f7a9bb9907
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-03-26 09:38:33 -0400

    add the other exercise

---
+++ ejercicios2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void intercambiar(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    int* vida = new int(100); 
+    
+    return 0;
+}
