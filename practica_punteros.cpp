6b7de86367e7165d04da913a2cf5c2932f08dad3
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-04-07 22:26:40 -0400

    add the function insert

--- practica_punteros.cpp
+++ practica_punteros.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 struct Nodo {
     int valor;
     Nodo *siguiente;
 };
+
+void clearScreen(){
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
 
 void insertar(Nodo*& cabeza, int valor){
     Nodo* nuevoNodo = new Nodo;
@@ -13,18 +22,66 @@ void insertar(Nodo*& cabeza, int valor){
     cabeza = nuevoNodo;
 };
 
+void mostrarNodo(Nodo* cabeza){
+    while (cabeza != nullptr)
+    {
+        cout << cabeza->valor << " "<< endl;
+        cabeza = cabeza->siguiente;
+    };
+    
+}
+
 main(){
     Nodo* cabeza = nullptr;
+    int option;
+    int valor;
+    while (true)
+    {
+        printf("BIENVENIDO AL PROGRAMA DE PUNTEROS\n");
+        printf("1. Insertar un nuevo nodo\n");
+        printf("2. Mostrar la lista\n");
+        printf("3. Salir\n");
+        printf("Ingrese una opcion: ");
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            clearScreen();
+            printf("Ingrese el valor del nodo: ");
+            cin >> valor;
+            insertar(cabeza, valor);
+            clearScreen();
+            break;
 
-    insertar(cabeza, 10);
-    insertar(cabeza, 20);
-    insertar(cabeza, 30);
+        case 2:
+            clearScreen();
+            mostrarNodo(cabeza);
+            printf("Presione enter para continuar...");
+            cin.ignore();
+            cin.get();
+            clearScreen();
+            break;  
+        
+        case 3:
+            printf("Saliendo del programa...\n");
+            return 0;
+        
+        default:
+            break;
+        }
+        
+    };
+    
 
-    Nodo* actual = cabeza;
-    while (actual != nullptr) {
-        cout << actual->valor << " ";
-        actual = actual->siguiente;
-    }
-    cout << endl;
+    // insertar(cabeza, 10);
+    // insertar(cabeza, 20);
+    // insertar(cabeza, 30);
+
+    // Nodo* actual = cabeza;
+    // while (actual != nullptr) {
+    //     cout << actual->valor << " ";
+    //     actual = actual->siguiente;
+    // }
+    // cout << endl;
 
 }
