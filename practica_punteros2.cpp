bd49b1b6d7e16d2ed71fa79f9e58fb0da811acf2
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-04-08 10:51:06 -0400

    add the exercise of pointers

--- practica_punteros2.cpp
+++ practica_punteros2.cpp
@@ -7,7 +7,7 @@ struct Lista{
 };
 
 int main(){
-    Lista *P, *Q;
+    Lista *P, *Q, *R;
     P = new Lista;
     P->dato = 15;
     P->siguiente = nullptr;
@@ -15,6 +15,10 @@ int main(){
     Q->dato = 20;
     Q->siguiente = nullptr;
     P->siguiente = Q;
+    R = new Lista;
+    R->dato = 25;
+    R->siguiente = nullptr;
+    Q->siguiente = R;
     Lista *aux = P;
     while (aux != nullptr)
     {
