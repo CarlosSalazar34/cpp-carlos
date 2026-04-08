8be800f4145ef2e315ab05eb9b0f3901c446b389
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-04-08 15:11:56 -0400

    add the iteration of pointers

--- clase3.cpp
+++ clase3.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+struct Lista{ 
+    int dato;
+    Lista* siguiente;
+};
+
 int main(){
+    Lista *primerPuntero, *segundoPuntero, *tercerPuntero;
+    primerPuntero = new Lista;
+    primerPuntero->dato = 15;
+    primerPuntero->siguiente = nullptr;
+    segundoPuntero = new Lista;
+    segundoPuntero->dato = 20;
+    segundoPuntero->siguiente = nullptr;
+    primerPuntero->siguiente = segundoPuntero;
+    Lista *auxiliar = primerPuntero;
+    while (auxiliar != nullptr)
+    {
+        cout << auxiliar->dato << endl;
+        auxiliar = auxiliar->siguiente;
+        /* code */
+    }
+    
     cout << "hola mundo" << endl;
     return 0;
 }
