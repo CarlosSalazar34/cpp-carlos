daad2ed6a49bf3876cde499d7b72d352421f1e1e
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-04-12 13:08:44 -0400

    add the fix of the insert function

--- ejercicios/exercise_one.cpp
+++ ejercicios/exercise_one.cpp
@@ -1,64 +1,60 @@
 #include <iostream>
 using namespace std;
 
-struct Nodo{ 
+struct Nodo { 
     int valor;
     Nodo* siguiente;
 };
 
-bool listaVacia(Nodo* cabeza){
+bool listaVacia(Nodo* cabeza) {
     return cabeza == nullptr;
-};
+}
 
-Nodo* crearNodo(int valor){
+Nodo* crearNodo(int valor) {
     Nodo* item = new Nodo;
     item->valor = valor;
     item->siguiente = nullptr;
     return item;
 }
 
-void insertarValor(Nodo*& cabeza, int valor){ 
+void insertarValor(Nodo*& cabeza, int valor) { 
     Nodo* nuevo = crearNodo(valor);
-    if (listaVacia(cabeza)){
+    if (listaVacia(cabeza)) {
         cabeza = nuevo;
-    }else { 
+    } else { 
         Nodo* aux = cabeza;
-        while (aux != nullptr)
-        {
+        // CORRECCIÓN: Nos detenemos en el último nodo real, no en el vacío
+        while (aux->siguiente != nullptr) {
             aux = aux->siguiente;
         }
+        // Ahora aux es el último, podemos colgar el nuevo nodo
         aux->siguiente = nuevo;
     }
 }
 
-void mostrarLista(Nodo* cabeza){ 
-    if (listaVacia(cabeza)){
-        cout << "La lista esta vacia "<< endl;
-    }else { 
+void mostrarLista(Nodo* cabeza) { 
+    if (listaVacia(cabeza)) {
+        cout << "La lista esta vacia" << endl;
+    } else { 
         Nodo* aux = cabeza;
-        while (aux != nullptr)
-        {
-            cout << aux->valor <<endl;
+        while (aux != nullptr) {
+            cout << "[" << aux->valor << "] -> ";
             aux = aux->siguiente;
         }
         cout << "NULL" << endl;
     }
 }
 
-void retornarValor(Nodo*& cabeza, int posicion){
-    if (listaVacia(cabeza)){ 
-        cout << "No hay valores en la lista" << endl;
-    }else { 
-        while (cabeza != nullptr)
-        {
-            cout << cabeza->valor << endl;
-            cabeza =  cabeza->siguiente;  
-        };
-        
-    }
-};
+int main() {
+    // Es vital inicializar en nullptr
+    Nodo* miLista = nullptr;
 
-int main(){
+    insertarValor(miLista, 10);
+    insertarValor(miLista, 20);
+    insertarValor(miLista, 30);
+
+    cout << "Contenido de la lista:" << endl;
+    mostrarLista(miLista);
 
     return 0;
 }
