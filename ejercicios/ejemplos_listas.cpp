9387f9743399808567456e4129b71b6fca6186b5
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-04-15 10:39:37 -0400

    add exercise of class

--- ejercicios/ejemplos_listas.cpp
+++ ejercicios/ejemplos_listas.cpp
@@ -1,60 +1,196 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
-struct Nodo { 
-    int valor;
-    Nodo* siguiente;
+struct Nodo {
+    int dato;
+    Nodo *prox;
 };
 
-bool listaVacia(Nodo* cabeza) {
-    return cabeza == nullptr;
+Nodo *crearNodo(int valor) {
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = valor;
+    nuevo->prox = NULL;
+    return nuevo;
 }
 
-Nodo* crearNodo(int valor) {
-    Nodo* item = new Nodo;
-    item->valor = valor;
-    item->siguiente = nullptr;
-    return item;
+bool listaVacia(Nodo *inicio) {
+    return inicio == NULL;
 }
 
-void insertarValor(Nodo*& cabeza, int valor) { 
-    Nodo* nuevo = crearNodo(valor);
-    if (listaVacia(cabeza)) {
-        cabeza = nuevo;
-    } else { 
-        Nodo* aux = cabeza;
-        // CORRECCIÓN: Nos detenemos en el último nodo real, no en el vacío
-        while (aux->siguiente != nullptr) {
-            aux = aux->siguiente;
+void imprimirLista(Nodo *inicio) {
+    if (listaVacia(inicio)) {
+        cout << "Lista esta vacia" << endl;
+        return;
+    }
+    Nodo *mover = inicio;
+    while (mover != NULL) {
+        cout << mover->dato << "->";
+        mover = mover->prox;
+    }
+    cout << "NULL" << endl;
+}
+
+void insertarPrimero(Nodo *&inicio, int valor) {
+    Nodo *nuevo = crearNodo(valor);
+    nuevo->prox = inicio;
+    inicio = nuevo;
+}
+
+void insertarOrdenado(Nodo *&inicio, int valor) {
+    if (listaVacia(inicio) || valor < inicio->dato) {
+        insertarPrimero(inicio, valor);
+    } else {
+        Nodo *nuevo = crearNodo(valor);
+        Nodo *aux = inicio;
+        while (aux->prox != NULL && aux->prox->dato < valor) {
+            aux = aux->prox;
         }
-        // Ahora aux es el último, podemos colgar el nuevo nodo
-        aux->siguiente = nuevo;
+        nuevo->prox = aux->prox;
+        aux->prox = nuevo;
     }
 }
 
-void mostrarLista(Nodo* cabeza) { 
-    if (listaVacia(cabeza)) {
-        cout << "La lista esta vacia" << endl;
-    } else { 
-        Nodo* aux = cabeza;
-        while (aux != nullptr) {
-            cout << "[" << aux->valor << "] -> ";
-            aux = aux->siguiente;
+float porcentajeApariciones(Nodo *inicio, int valor) {
+    if (listaVacia(inicio)) return 0;
+    int contador = 0, total = 0;
+    Nodo *aux = inicio;
+    while (aux != NULL) {
+        if (aux->dato == valor) contador++;
+        total++;
+        aux = aux->prox;
+    }
+    return (total == 0) ? 0 : ((float)contador / total * 100);
+}
+
+Nodo* buscarPuntero(Nodo *inicio, int valor) {
+    Nodo *aux = inicio;
+    while (aux != NULL) {
+        if (aux->dato == valor) return aux;
+        aux = aux->prox;
+    }
+    return NULL;
+}
+
+void eliminarApariciones(Nodo *&inicio, int valor) {
+    while (inicio != NULL && inicio->dato == valor) {
+        Nodo *eliminar = inicio;
+        inicio = inicio->prox;
+        delete eliminar;
+    }
+    Nodo *actual = inicio;
+    while (actual != NULL && actual->prox != NULL) {
+        if (actual->prox->dato == valor) {
+            Nodo *eliminar = actual->prox;
+            actual->prox = eliminar->prox;
+            delete eliminar;
+        } else {
+            actual = actual->prox;
         }
-        cout << "NULL" << endl;
+    }
+}
+
+void vaciarLista(Nodo *&inicio) {
+    while (inicio != NULL) {
+        Nodo *aux = inicio;
+        inicio = inicio->prox;
+        delete aux;
+    }
+}
+
+void generarAleatorios(Nodo *&inicio, int n) {
+    for (int i = 0; i < n; i++) {
+        insertarOrdenado(inicio, rand() % 100); 
     }
 }
 
 int main() {
-    // Es vital inicializar en nullptr
-    Nodo* miLista = nullptr;
+    Nodo *miLista = NULL; 
+    int opcion, valor, n;
 
-    insertarValor(miLista, 10);
-    insertarValor(miLista, 20);
-    insertarValor(miLista, 30);
 
-    cout << "Contenido de la lista:" << endl;
-    mostrarLista(miLista);
+    srand((unsigned) time(NULL));
 
+    do {
+        cout << "1. Insertar ordenadamente" << endl;
+        cout << "2. Ver porcentaje de apariciones" << endl;
+        cout << "3. Buscar ubicacion (Puntero)" << endl;
+        cout << "4. Eliminar todas las apariciones" << endl;
+        cout << "5. Mostrar lista" << endl;
+        cout << "6. Vaciar lista (Reiniciar)" << endl;
+        cout << "7. Generar lista" << endl;
+        cout << "0. Salir" << endl;
+        cout << "Seleccione: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese valor: ";
+                cin >> valor;
+                insertarOrdenado(miLista, valor);
+                cout << "Insertado." << endl;
+                break;
+
+            case 2:
+                if (listaVacia(miLista)) {
+                    cout << "La lista esta vacia." << endl;
+                } else {
+                    cout << "Valor a calcular %: ";
+                    cin >> valor;
+                    float porc = porcentajeApariciones(miLista, valor);
+                    cout << "El valor " << valor << " aparece el " << porc << "% de las veces." << endl;
+                }
+                break;
+
+            case 3:
+                cout << "Valor para obtener puntero: ";
+                cin >> valor;
+                {
+                    Nodo* ptr = buscarPuntero(miLista, valor); 
+                    if (ptr) 
+                        cout << "Encontrado en la direccion de memoria: " << ptr << endl;
+                    else 
+                        cout << "Valor no encontrado." << endl;
+                }
+                break;
+
+            case 4:
+                cout << "Valor a eliminar por completo: ";
+                cin >> valor;
+                eliminarApariciones(miLista, valor);
+                cout << "Proceso de eliminacion terminado." << endl;
+                break;
+
+            case 5:
+                cout << "Contenido de la lista: ";
+                imprimirLista(miLista);
+                break;
+
+            case 6:
+                vaciarLista(miLista);
+                cout << "Memoria liberada. Lista reiniciada." << endl;
+                break;
+
+            case 7:
+                cout << "Ingrese la cantidad de elementos a generar: ";
+                cin >> n;
+                if (n > 0) {
+                    generarAleatorios(miLista, n);
+                    cout << n << " elementos aleatorios insertados ordenadamente." << endl;
+                } else {
+                    cout << "Cantidad no valida." << endl;
+                }
+                break; 
+
+            case 0:
+                cout << "Saliendo..." << endl;
+                break;
+
+            default:
+                cout << "Opcion no valida." << endl;
+        }
+    } while (opcion != 0);
+    vaciarLista(miLista);
     return 0;
 }
